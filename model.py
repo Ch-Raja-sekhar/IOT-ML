fee6d4ee5faf84e8fd69965c090ba0303ca8ee85
Ch-Raja-sekhar <24jr5a0506@gmail.com>
2025-09-08 14:09:02 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pickle
+
+def generateAI():
+    data=pd.read_csv('dataset.csv')
+    X=data.iloc[:,:-1].values
+    y=data.iloc[:,-1].values
+    X=X.reshape(-1,1)
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+    ai=KNeighborsClassifier(n_neighbors=5)
+    ai.fit(X_train,y_train)
+
+    y_ai=ai.predict(X_test)
+    accuracy_score(y_test,y_ai)
+    pickle.dump('ai.pkl','wb')
+    print("model is created")
