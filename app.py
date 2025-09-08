a79e653200449ce73b73ce33c1590cde1ad0150f
Ch-Raja-sekhar <24jr5a0506@gmail.com>
2025-09-08 15:34:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from flask import Flask, request
+from flask import Flask,request
 from model import generateAI
 import pickle
 
+
 generateAI()
 ai=pickle.load(open('ai.pkl','rb'))
-app=Flask(_name_)
+app=Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Server Running"
+    return "server Running"
 
-
-@app.route('/predict')
+@app.route("/predict")
 def predict():
-    ir=request.args.get('ir')
+    ir=request.args.get("ir")
     ir=int(ir)
     data=[[ir]]
-    result=ai.predict(data)[0] #['Object']
+    result=ai.predict(data)[0]
     return result
 
-if _name=='main_':
+if(__name__)=="__main__":
     app.run(host='0.0.0.0',port=4000)
